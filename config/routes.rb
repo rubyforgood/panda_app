1de5210cdec4cd334e23600915b4471c67ec06df
christinaforney <cforney@platinumperformance.com>
2016-06-18 10:38:36 -0400

    Removing extra routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root 'landing#index'
 
-  devise_for :users, :controllers =>
+  devise_for :users, :skip => [:registrations], :controllers =>
     {
       sessions: "users/sessions",
-      registrations: "users/registrations",
       omniauth_callbacks: "users/omniauth_callbacks"
     }
 
