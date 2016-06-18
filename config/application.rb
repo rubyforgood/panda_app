2b935d511d0e6e46a0fd9c3a66049ffaaadd165f
Betsy Haibel <betsy.haibel@gmail.com>
2016-06-17 20:33:23 -0400

    try babelify in pipeline

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,6 @@ module PandaApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.browserify_rails.commandline_options = "-t mithrilify"
+    config.browserify_rails.commandline_options = "-t mithrilify -t babelify"
   end
 end
