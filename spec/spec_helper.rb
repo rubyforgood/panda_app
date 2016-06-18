08bea4a4dbda51c07501894274266b4fb4527426
Andrew Purcell <andrew.purcell@grandrounds.com>
2016-06-18 11:45:49 -0400

    Update test config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -89,4 +89,6 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.include FactoryGirl::Syntax::Methods
+  config.use_transactional_fixtures = true
 end
