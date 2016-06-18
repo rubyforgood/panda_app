40a7dcc8d08ce79750deaaadf1d5407f1cdb7344
Betsy Haibel <betsy.haibel@gmail.com>
2016-06-18 11:09:39 -0400

    stubby scheme repository

--- app/assets/javascripts/repositories/scheme_repository.js
+++ app/assets/javascripts/repositories/scheme_repository.js
@@ -2,16 +2,24 @@ const Scheme = require('./../models/scheme');
 
 const SchemeRepository = {
   all: function() {
-    return [
-      new Scheme()
-    ];
+    return SchemeRepository.cache.values();
   },
 
   get: function(uuid) {
-    return new Scheme({uuid: uuid});
+    return SchemeRepository.cache[uuid] || new Scheme({uuid: uuid});
   },
 
-  save: function(scheme) {
+  save: function(uuid) {
+    return SchemeRepository.cache[uuid];
+  },
+
+  new: function(params) {
+    var scheme = new Scheme(params);
+    scheme.addBlankSubject();
+    scheme.addBlankBehavior();
+    SchemeRepository.cache[scheme.uuid] = scheme;
     return scheme;
-  }
+  },
+
+  cache: {}
 }
