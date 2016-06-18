c414cdf2168583ab98b130293db155d1275f1657
Andrew Purcell <andrew.purcell@grandrounds.com>
2016-06-18 17:04:55 -0400

    Upsert on observation_sessions#create

--- app/controllers/api/observation_sessions_controller.rb
+++ app/controllers/api/observation_sessions_controller.rb
@@ -1,9 +1,10 @@
 module Api
   class ObservationSessionsController < ApplicationController
     def create
-      @observation_session = ObservationSession.create(create_params)
+      @observation_session = ObservationSession.find_or_initialize_by(id: id_or_nil)
+      @observation_session.assign_attributes(create_params)
 
-      if @observation_session
+      if @observation_session.save
         head 201
       else
         head 422
@@ -19,12 +20,16 @@ module Api
 
     private
 
+    def id_or_nil
+      params.permit(:id)[:id]
+    end
+
     def observation_session_id
       params.require(:id)
     end
 
     def create_params
-      filtered_create_params.merge(metadata: extracted_metadata, user: current_user)
+      filtered_create_params.merge(metadata: extracted_metadata, user_id: current_user.id)
     end
 
     def filtered_create_params
@@ -33,7 +38,14 @@ module Api
         .permit(:id, :name, :observation_method,
                 :session_interval_seconds, :session_duration_seconds,
                 :started_at, :focal_animal_id, :focal_behavior_id,
-                :scheme_id, :notes, :metadata)
+                :scheme_id, :notes, :metadata,
+                observations_attributes: [
+                  :started_at, :event_type, :duration_seconds,
+                  :time_lag_seconds, :notes,
+                  :behavior_id, :actor_id, :receiver_id, :id,
+                  modifiers: []
+                ]
+               )
     end
 
     def extracted_metadata
