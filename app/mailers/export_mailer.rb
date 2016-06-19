b07d012df352c5d1daf680d751b34e549a05428a
christinaforney <cforney@platinumperformance.com>
2016-06-18 21:43:22 -0400

    Cleaning up ExportMailer

--- app/mailers/export_mailer.rb
+++ app/mailers/export_mailer.rb
@@ -1,14 +1,18 @@
 class ExportMailer < ActionMailer::Base
+
+  class Attachment < DelegateClass(StringIO)
+    def path
+      "observation_report.csv"
+    end
+  end
+
   def export_email(user, csv_string)
     @user = user
     mg_client = Mailgun::Client.new ENV["MAILGUN_API_KEY"]
     html_output = render_to_string template: "export_mailer/export_email.html.erb"
     text_output = render_to_string template: "export_mailer/export_email.text.erb"
 
-    attachment = StringIO.new(csv_string)
-    def attachment.path
-      "observtion_report.csv"
-    end
+    attachment = Attachment.new(StringIO.new(csv_string))
 
     message_params = {:from    => "no-reply@panda-app.herokuapps.com",
                       :to      => @user.email,
