88042149fcc8086827b96adeea31a5211fa0f07e
colleenmcguckin <colleenmcguckin@gmail.com>
2016-06-19 12:03:32 -0400

    generate csv with ObservationExporter

--- lib/observation_exporter.rb
+++ lib/observation_exporter.rb
@@ -1,6 +1,30 @@
 class ObservationExporter
   def self.generate_observation_csv(observation_session)
-    # TODO: This needs to be implemented - should return a string of csv data
-    "foo, bar"
+    csv_output = headers
+    observation_session.observations.each do |observation|
+      csv_output << '\n'
+      csv_output << row(observation)
+    end
+    return csv_output
+  end
+
+  private
+  def self.headers
+    "started at, event type,
+    duration, time lag (s),
+    modifiers, notes, behavior,
+    actor, receiver"
+  end
+
+  def self.row(observation)
+    "#{observation['started_at']},
+    #{observation['event_type']},
+    #{observation['duration_seconds']},
+    #{observation['time_lag_seconds']},
+    #{observation['modifiers']&.to_sentence},
+    #{observation['notes']},
+    #{Behavior.find(observation['behavior_id'])&.name},
+    #{Subject.find(observation['actor_id'])&.name},
+    #{Subject.where(id: observation['receiver_id']).first&.name}"
   end
 end
