7ee852e105e65ca031638b8672fd15a13615d376
christinaforney <cforney@platinumperformance.com>
2016-06-18 23:57:24 -0400

    Adding development authentication

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,2 +1,13 @@
 class Users::SessionsController < Devise::SessionsController
+  def create
+    if Rails.env.development?
+      email = params[:email]
+      user = User.find_or_initialize_by(email: email)
+      user.save(validate: false)
+      sign_in user
+      redirect_to root_path
+    else
+      super
+    end
+  end
 end
