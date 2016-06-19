396ae58bac24c76328a6c385e62f173a9c77d2c6
christinaforney <cforney@platinumperformance.com>
2016-06-19 13:06:29 -0400

    Fixing newlines in observation exporter

--- lib/observation_exporter.rb
+++ lib/observation_exporter.rb
@@ -1,30 +1,24 @@
 class ObservationExporter
   def self.generate_observation_csv(observation_session)
-    csv_output = headers
-    observation_session.observations.each do |observation|
-      csv_output << '\n'
-      csv_output << row(observation)
-    end
-    return csv_output
+    headers.concat(observation_session.observations
+      .map {|observation| row(observation) }
+      .join("\n"))
   end
 
   private
   def self.headers
-    "started at, event type,
-    duration, time lag (s),
-    modifiers, notes, behavior,
-    actor, receiver"
+    "started at, event type, duration, time lag (s), modifiers, notes, behavior, actor, receiver\n"
   end
 
   def self.row(observation)
-    "#{observation['started_at']},
-    #{observation['event_type']},
-    #{observation['duration_seconds']},
-    #{observation['time_lag_seconds']},
-    #{observation['modifiers']&.join(' ')},
-    #{observation['notes']},
-    #{Behavior.find(observation['behavior_id'])&.name},
-    #{Subject.find(observation['actor_id'])&.name},
-    #{Subject.where(id: observation['receiver_id']).first&.name}"
+    [observation['started_at'],
+    observation['event_type'],
+    observation['duration_seconds'],
+    observation['time_lag_seconds'],
+    observation['modifiers']&.join(' '),
+    observation['notes'],
+    Behavior.find(observation['behavior_id'])&.name,
+    Subject.find(observation['actor_id'])&.name,
+    Subject.where(id: observation['receiver_id']).first&.name].join(',')
   end
 end
